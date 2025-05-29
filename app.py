6338aa40b7e2f786cbd9b02aea3fc8b83b1c2ae0
engJoseSantos <eng.josesantos97@gmail.com>
2025-05-29 21:55:22 +0200

    add: mp3 option
    Add the mp3 download option

--- app.py
+++ app.py
@@ -2,22 +2,52 @@
 # https://github.com/engJoseSantos
 
 
-
-# from pytube import YouTube
+# from pytube import YouTube # Old library
 from pytubefix import YouTube
+from moviepy import AudioFileClip
+import os
 
 
-def Download(link):
+def sanitize_filename(name):
+    # TO-CHANGE to regex
+    return "".join(c for c in name if c.isalnum() or c in " ._-").strip() 
+
+def download_video(link):
     # Create the youtube object
     youtubeObject = YouTube(link)
-    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    videoStream = youtubeObject.streams.get_highest_resolution()
     try:
         # Make the download
-        youtubeObject.download()
+        videoStream.download(filename=sanitize_filename(youtubeObject.title) + ".mp4")
+    except:
+        print("An error has occurred")
+    print("Download is completed successfully")
+
+def download_audio(link):
+    # Create the youtube object
+    youtubeObject = YouTube(link)
+    audioStream = youtubeObject.streams.filter(only_audio=True).first()
+    filename = sanitize_filename(youtubeObject.title)
+    try:
+        # Make the download
+        downloaded_file = audioStream.download(filename=sanitize_filename(youtubeObject.title) + ".mp4")
+    except:
+        print("An error has occurred")
+    print("Try to convert")
+
+    try:
+        # Converte para mp3
+        clip = AudioFileClip(downloaded_file)
+        clip.write_audiofile(filename + ".mp3")
+        clip.close()
+        os.remove(downloaded_file)  # remove the file
     except:
         print("An error has occurred")
     print("Download is completed successfully")
 
 
+
+
 link = input("Enter the YouTube video URL: ")
-Download(link)
+download_audio(link)
+#download_video(link)
